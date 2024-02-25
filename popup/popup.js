94d922dade90154211149a5945c139753291ac89
emsesc <emseschen@gmail.com>
2024-02-25 10:01:02 -0500

    add title

--- popup/popup.js
+++ popup/popup.js
@@ -5,13 +5,18 @@ chrome.storage.local.get(['emailData'], function (result) {
     const phishingSigns = emailData.phishingSigns;
     const mainPhishingSign = phishingSigns[phishingSigns.length - 1]; // Get the last value
     document.getElementById('phishing-sign').innerText = mainPhishingSign;
+    console.log(phishingSigns.slice(0, -1))
     highlightPhrases(phishingSigns.slice(0, -1)); // Highlight all other values
   }
 });
 
 function highlightPhrases(phrases) {
+  console.log(phrases);
   phrases.forEach(phrase => {
-    const regex = new RegExp(phrase, 'gi');
-    document.body.innerHTML = document.body.innerHTML.replace(regex, `<span style="background-color: yellow;">${phrase}</span>`);
+    phrase = phrase.trim().replace('- ', '');
+    if (phrase !== '') {
+      const regex = new RegExp(phrase, 'gi');
+      document.body.innerHTML = document.body.innerHTML.replace(regex, `<span style="background-color: yellow;">${phrase}</span>`);
+    }
   });
 }

306856b18d5c28c271c3e6948442907f910c4632
Alicia <alicia.bao@unc.edu>
2024-02-25 09:38:56 -0500

    aa

--- background.js
+++ background.js
@@ -1,38 +1,46 @@
 // Listen for a click on the extension icon
 chrome.tabs.onUpdated.addListener(async (tabId) => {
   // Execute content script to scrape email data
-  const emailData = await scrapeEmail(tabId);
+  await scrapeEmail(tabId);
+  console.log("scrape called");
+  const data = chrome.local.storage.get(['key']);
   // Send POST request
-  sendPOSTRequest(emailData);
+  sendPOSTRequest(data);
+  console.log("opst sent");
 });
 
 async function scrapeEmail(tabId) {
-  return new Promise(resolve => {
-    chrome.scripting.executeScript({
-      target: { tabId: tabId },
-      function: scrapeEmailFromPage
-    }, (results) => {
-      if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError.message);
-        return;
-      }
-      const emailData = results[0].result;
-      resolve(emailData);
-    });
-  });
+  chrome.tabs.sendMessage(tabId, {
+    action: 'callScrape'
+  })
+  // return new Promise(resolve => {
+  //   chrome.scripting.executeScript({
+  //     target: { tabId: tabId },
+  //     function: scrapeEmailFromPage
+  //   }, (results) => {
+  //     if (chrome.runtime.lastError) {
+  //       console.error(chrome.runtime.lastError.message);
+  //       return;
+  //     }
+  //     const emailData = results[0].result;
+  //     resolve(emailData);
+  //   });
+  // });
 }
 
-function scrapeEmailFromPage() {
-  const emailSubjectElement = document.querySelector('[data-testid="email-subject"]');
-  const emailBodyElement = document.querySelector('[data-testid="email-body"]');
-  const emailSenderElement = document.querySelector('[data-testid="email-sender"]');
+// function scrapeEmailFromPage() {
+//   const emailSubjectElement = document.querySelector('[data-testid="email-subject"]');
+//   // selects text w no tags 
+//   const emailBodyElement = document.querySelector('.PlainText');
+//   console.log("email bod", emailBodyElement);
+//   const emailSenderElement = document.querySelector('[data-testid="email-sender"]');
 
-  const emailSubject = emailSubjectElement ? emailSubjectElement.innerText : '';
-  const emailBody = emailBodyElement ? emailBodyElement.innerText : '';
-  const emailSender = emailSenderElement ? emailSenderElement.innerText : '';
+//   const emailSubject = emailSubjectElement ? emailSubjectElement.innerText : '';
+//   const emailBody = emailBodyElement ? emailBodyElement.innerText : '';
+//   const emailSender = emailSenderElement ? emailSenderElement.innerText : '';
 
-  return { subject: emailSubject, body: emailBody, sender: emailSender };
-}
+//   return { subject: emailSubject, body: emailBody, sender: emailSender };
+// }
 
 function sendPOSTRequest(emailData) {
   const url = 'http://127.0.0.1:5000/phishing_detection';
--- popup/popup.js
+++ popup/popup.js
@@ -4,10 +4,33 @@ chrome.storage.local.get(['emailData'], function (result) {
   if (emailData && emailData.phishingSigns) {
     const phishingSigns = emailData.phishingSigns;
     const mainPhishingSign = phishingSigns[phishingSigns.length - 1]; // Get the last value
-    document.getElementById('phishing-sign').innerText = mainPhishingSign;
+    document.getElementById('phishing-sign').innerHTML = mainPhishingSign;
+    console.log("innerhtml set");
     highlightPhrases(phishingSigns.slice(0, -1)); // Highlight all other values
   }
 });
+
+chrome.onRunTime.onMessage.addListener(function(msg) {
+    if (msg.action == 'callScrape'){
+        let data = callScrape();
+        chrome.local.storage.set({key: data});
+        console.log('data stored');
+    }
+})
+
+function callScrape() {
+const emailSubjectElement = document.querySelector('[data-testid="email-subject"]');
+  // selects text w no tags 
+  const emailBodyElement = document.querySelector('.PlainText');
+  console.log("email bod", emailBodyElement);
+  const emailSenderElement = document.querySelector('[data-testid="email-sender"]');
+
+  const emailSubject = emailSubjectElement ? emailSubjectElement.innerText : '';
+  const emailBody = emailBodyElement ? emailBodyElement.innerText : '';
+  const emailSender = emailSenderElement ? emailSenderElement.innerText : '';
+
+  return { subject: emailSubject, body: emailBody, sender: emailSender };
+}
 
 function highlightPhrases(phrases) {
   phrases.forEach(phrase => {
